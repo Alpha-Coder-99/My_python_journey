12850641a23a0f71c937637217050d31a6328490
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-03-03 00:08:15 +0500

    string understood with Mini project Emoji convertor

---
+++ Chap#6_Strings/Practice_Question#4.py
@@ -0,0 +1,9 @@
+# Write a prgram that:
+# Take a sentaence as input 
+# converts in lower case
+# Replce all sapces in _ 
+# print the new string
+str=input("Enter a sentence") #Alpha Coder is happy today
+print(str.lower())
+newstr=str.replace(" ","_")
+print(newstr) 
