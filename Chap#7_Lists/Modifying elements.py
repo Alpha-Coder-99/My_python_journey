b4e66a59a0538e565a084cb8bcf7fafb1c163579
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-03-04 21:25:39 +0500

    List

---
+++ Chap#7_Lists/Modifying elements.py
@@ -0,0 +1,9 @@
+skills = ["HTML", "CSS", "Python", "AI"]
+
+# HTML aur CSS ki jagah 'Freelancing' aur 'Soft Skills' dal dia
+skills[0:2] = ["Freelancing", "Communication"]
+print(skills)
+
+fruits=["Mango","Apple","Banana","Orange","Strawberry",8,5.0]
+fruits[2:3]=["Water melon","Dates"]
+print(fruits)
---
+++ Chap#7_Lists/list_indexing.py
@@ -0,0 +1,6 @@
+fruits=["Mango","Apple","Banana","Orange","Strawberry",8,5.0]
+print("First value of the list:",fruits[0]) 
+print("Second value of the list:",fruits[1]) 
+#Negative indexing
+print(fruits[-1]) 
+print(fruits[-2]) 
