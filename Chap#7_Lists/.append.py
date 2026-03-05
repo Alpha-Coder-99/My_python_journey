2f9587df318db85ccbac77564c8468b7582b5d5d
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-03-05 23:26:53 +0500

    list function

---
+++ Chap#7_Lists/.append.py
@@ -0,0 +1,6 @@
+shopping_cart = ["flour", "sugar", "Chaye"]
+print("first list:", shopping_cart)
+shopping_cart.append("Doodh")
+shopping_cart.append("Anday")
+print(" list after appending:", shopping_cart)  
+
