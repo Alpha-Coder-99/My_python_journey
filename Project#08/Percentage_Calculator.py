9c2c4fc7a4abe2356e26552c7880a260d4650537
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-04-06 15:45:21 +0500

    Percentage calculator🧮

---
+++ Project#08/Percentage_Calculator.py
@@ -0,0 +1,7 @@
+obtained_marks = 900
+total_marks = 1200
+
+# Percentage calculate karne ka formula
+percentage = (obtained_marks / total_marks) * 100
+
+print(f"Aapki percentage hai: {percentage:.2f}%")
