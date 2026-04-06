00783cca5c350381fa7f7d0715aa77471b4c81b1
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-04-06 16:01:23 +0500

    MY Percentage Calculator %🧮👧🥰🥰

--- Project#08/Percentage_Calculator.py
+++ Project#08/Percentage_Calculator.py
@@ -1,7 +1,19 @@
-obtained_marks = 900
-total_marks = 1200
+# obtained_marks = 900
+# total_marks = 1200
 
-# Percentage calculate karne ka formula
-percentage = (obtained_marks / total_marks) * 100
+# # Percentage calculate karne ka formula
+# percentage = (obtained_marks / total_marks) * 100
 
-print(f"Aapki percentage hai: {percentage:.2f}%")
+# print(f"Aapki percentage hai: {percentage:.2f}%")
+
+def calculate( obtained_marks, total_marks):
+    return (obtained_marks / total_marks) * 100
+
+marks = float(input("Marks: "))
+total = float(input("Total: "))
+
+result=calculate(marks, total)
+print(f"Aapki percentage hai: {result:.2f}%",)
+    
+
+
