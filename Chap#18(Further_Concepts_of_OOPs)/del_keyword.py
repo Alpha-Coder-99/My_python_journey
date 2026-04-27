314d40bcba227b0cb8fa0c636c2c39f89b669907
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-04-27 20:16:29 +0500

    Further_Concepts_of_OOPs

---
+++ Chap#18(Further_Concepts_of_OOPs)/del_keyword.py
@@ -0,0 +1,14 @@
+#Use to delete object properties or object itself
+# del s1.name
+# del s1
+
+class Student:
+    def __init__(self,name):
+        self.name=name
+
+s1=Student("Halima")
+print(s1.name)
+
+del s1
+print(s1.name)
+        
