cbb6d7d0236bad440a8d62053ce6bcd1d2376962
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-04-27 17:38:15 +0500

    Clear all Concept of OOPs

--- Chap#15(OOP)/_ _init_ _.py().py
+++ Chap#15(OOP)/_ _init_ _.py().py
@@ -7,17 +7,35 @@ Jaise hi koi bacha paida hota hai,
  uska naam aur rang pehle se tay hota hai,
  waise hi __init__ object ko uski pehli values (attributes) deta hai."""
 
-class Student:
-    name="Raha"
-    def __init__(self):
-        print("Adding new student in Database...")
-        print(self)
+# class Student:
+#     name="Raha"
+#     def __init__(self):
+#         print("Adding new student in Database...")
+#         print(self)
     
 
-# print("student")
+# # print("student")
 
-#Creating Objects/instances of classes
-s1=Student()
-print(s1.name)
-# s2=Student()
-# print(s2.name)
+# # Creating Objects/instances of classes
+# s1=Student()
+# print(s1.name)
+# # s2=Student()
+# # print(s2.name)
+
+
+
+
+class Student:
+
+    def __init__(self,fullname,marks):
+        self.fullname=fullname
+        self.marks=marks
+        print("adding new student in  data base...  ")
+
+s1=Student("Raha",56)
+print(s1.fullname,s1.marks)
+s2=Student("Batool",78)
+print(s2.fullname,s2.marks)
+s3=Student("Sara",45)
+print(s3.fullname,s3.marks)
+        
---
+++ Chap#15(OOP)/class&InstanceAtribues.py
@@ -0,0 +1,12 @@
+class Student:
+    school="Inspire"#class attributes
+    name="Kiran"
+    def __init__(self,fullname,marks):
+        self.fullname=fullname #object attributes
+        self.marks=marks
+        print("adding new student in  data base...  ")
+
+s1=Student("Raha",56)
+print(s1.fullname,s1.marks,Student.school)
+s2=Student("Batool",78)
+print(s2.fullname,s2.marks,Student.school)
