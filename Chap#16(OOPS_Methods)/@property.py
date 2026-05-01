9b055bb078076dc1260730c37524d8ada2a34ca1
Alpha-Coder-99 <gamingarena*2@gmail.com>
2026-05-01 22:06:35 +0500

    OOPs methods

---
+++ Chap#16(OOPS_Methods)/@property.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,phy,math,com):
+        self.phy=phy
+        self.com=com
+        self.math=math
+
+    @property
+    def percentage(self):
+        return str((self.phy+self.com+self.math)/3)+"%"
+stu1=Student(81,98,78)
+print(stu1.percentage)
+
+stu1.phy=88
+print(stu1.percentage)
+
+        
---
+++ Chap#16(OOPS_Methods)/Class_methods.py
@@ -0,0 +1,19 @@
+# class person :
+#     name="anonymous"
+
+#     def ChangeName(self,name):
+#         self.__class__.name="Batool"
+# p1=person()
+# p1.ChangeName("Batool")
+# print(p1.name)
+# print(person.name)
+#IN methods way
+class person :
+    name="anonymous"
+    @classmethod
+    def ChangeName(cls,name):
+        cls.name=name
+p1=person()
+p1.ChangeName("Batool")
+print(p1.name)
+print(person.name)
